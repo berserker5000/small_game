b022e11df666fcfceb288c2d9bad56ac390fe174
berserker5000 <dima.shaman.k76@gmail.com>
2017-04-27 15:13:28 +0200

    prompts added

--- v2/game.py
+++ v2/game.py
@@ -1,4 +1,54 @@
-from libraries import generate_number, check_winner, do_move, get_users, do_exit, bcolors
+from libraries import generate_number, bcolors, number_prompt
+
+
+def check_winner(user, user_number, reserved_number):
+    if reserved_number == user_number:
+        print (bcolors.BOLD + str(user).capitalize() + " you won!" + bcolors.ENDC + '\n')
+        return True
+    elif user_number > reserved_number:
+        print (str(
+            user) + " your number is " + bcolors.BOLD + "greater" + bcolors.ENDC + " than was guessed. Next try.")
+    elif user_number < reserved_number:
+        print (str(
+            user) + " your number is " + bcolors.BOLD + "less" + bcolors.ENDC + " than was guessed. Next try.")
+    return False
+
+
+def get_users():
+    # n = ask_number("Get me number: ", min, max)
+    while True:
+        try:
+            user_name = []
+            users_in_game = raw_input("Enter number of players: ")
+            for i in range(0, int(users_in_game)):
+                user = raw_input("Enter " + str(i + 1) + " user name: ")
+                user_name.append(user)
+            return user_name
+        except ValueError:
+            print "Value Error!"
+
+
+def do_move(user):
+    while True:
+        try:
+            your_number = int(raw_input(str(user) + " please enter your number: "))
+            if validate_number(your_number):
+                return your_number
+            else:
+                print "You entered negative number. Game is only with positive. Try again."
+        except ValueError:
+            print "Sorry, you entered not a number. Please, try again."
+
+
+def do_exit():
+    while True:
+        answer = raw_input("Do you want to play again? (Y/n): ")
+        if not answer or answer.lower() in ('y', 'yes'):
+            return True
+        elif answer.lower() in ('n', 'no'):
+            return False
+        else:
+            print("Not a valid answer!")
 
 
 def start_game(users):
@@ -6,9 +56,9 @@ def start_game(users):
     move = 0
     while True:
         for user in users:
-            move +=1
+            move += 1
             if check_winner(user, do_move(user), computer_number):
-                print bcolors.BOLD + user + " won this game in "+ str(move) + " moves. Congratulates!" + bcolors.ENDC
+                print bcolors.BOLD + user + " won this game in " + str(move) + " moves. Congratulates!" + bcolors.ENDC
                 return True
 
 
--- v2/libraries.py
+++ v2/libraries.py
@@ -22,8 +22,13 @@ def text_prompt(text):
     pass
 
 
-def number_prompt(number):
-    pass
+def number_prompt(ask, **minimum, **maximum):
+    while True:
+        prompt = raw_input(ask)
+        if validate_number(prompt):
+            if prompt>minimum and prompt<maximum:
+                return prompt
+
 
 
 def add_user(**users):
@@ -35,65 +40,22 @@ def remove_user(user):
 
 
 def help():
-    print "This a help message. \n You can you such commands as: \n " \
+    return "This a help message. \n You can you such commands as: \n " \
           "/add <user_name> - this command will add user to your game. \n " \
           "/remove <user_name> - this command will remove user from playing. \n " \
           "help - this command will display current message."
 
 
-def do_exit():
-    while True:
-        answer = raw_input("Do you want to play again? (Y/n): ")
-        if not answer or answer.lower() in ('y', 'yes'):
-            return True
-        elif answer.lower() in ('n', 'no'):
-            return False
-        else:
-            print("Not a valid answer!")
+
 
 
 def generate_number():
-    rnd = random.randint(1, 100)
-    computer_number = random.randint(1, rnd)
+    #rnd = random.randint(1, 100)
+    computer_number = random.randint(1, 100)
     return computer_number
 
 
-def check_winner(user, user_number, reserved_number):
-    if reserved_number == user_number:
-        print (bcolors.BOLD + str(user).capitalize() + " you won!" + bcolors.ENDC + '\n')
-        return True
-    elif user_number > reserved_number:
-        print (str(
-            user) + " your number is " + bcolors.BOLD + "greater" + bcolors.ENDC + " than was guessed. Next try.")
-    elif user_number < reserved_number:
-        print (str(
-            user) + " your number is " + bcolors.BOLD + "less" + bcolors.ENDC + " than was guessed. Next try.")
-    return False
 
-
-def get_users():
-    while True:
-        try:
-            user_name = []
-            users_in_game = raw_input("Enter number of players: ")
-            for i in range(0, int(users_in_game)):
-                user = raw_input("Enter " + str(i + 1) + " user name: ")
-                user_name.append(user)
-            return user_name
-        except ValueError:
-            print "Value Error!"
-
-
-def do_move(user):
-    while True:
-        try:
-            your_number = int(raw_input(str(user) + " please enter your number: "))
-            if validate_number(your_number):
-                return your_number
-            else:
-                print "You entered negative number. Game is only with positive. Try again."
-        except ValueError:
-            print "Sorry, you entered not a number. Please, try again."
 
 
 class bcolors:
