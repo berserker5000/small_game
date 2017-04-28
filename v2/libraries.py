ec34168d4eb644cffec9035022a468a34c9b963d
berserker5000 <dima.shaman.k76@gmail.com>
2017-04-28 12:44:05 +0200

    prompts added

--- v2/libraries.py
+++ v2/libraries.py
@@ -18,17 +18,41 @@ def commands(command):
     pass
 
 
-def text_prompt(text):
-    pass
+def bold(text):
+    if check_not_empty(text) == False:
+        return text
+    return bcolors.BOLD + str(text) + bcolors.ENDC
 
 
-def number_prompt(ask, **minimum, **maximum):
+def check_not_empty(text):
+        if str(text).isspace() or str(text) == "":
+            return False
+        return True
+
+def check_name_not_empty(text, error_message):
+        if not check_not_empty(text):
+            print str(error_message)
+            return False
+        return True
+
+def check_name_in_list(lst):
+    for i in lst:
+        pass
+
+
+
+
+
+def number_prompt(ask, minimum, maximum):
     while True:
         prompt = raw_input(ask)
         if validate_number(prompt):
-            if prompt>minimum and prompt<maximum:
+            if prompt > minimum and prompt < maximum:
                 return prompt
-
+            elif prompt < minimum:
+                return "Sorry, our game is starts from " + str(minimum)
+            elif prompt > maximum:
+                return "Sorry, game is valid till " + str(maximum)
 
 
 def add_user(**users):
@@ -41,21 +65,15 @@ def remove_user(user):
 
 def help():
     return "This a help message. \n You can you such commands as: \n " \
-          "/add <user_name> - this command will add user to your game. \n " \
-          "/remove <user_name> - this command will remove user from playing. \n " \
-          "help - this command will display current message."
-
-
-
+           "/add <user_name> - this command will add user to your game. \n " \
+           "/remove <user_name> - this command will remove user from playing. \n " \
+           "help - this command will display current message."
 
 
 def generate_number():
-    #rnd = random.randint(1, 100)
+    # rnd = random.randint(1, 100)
     computer_number = random.randint(1, 100)
     return computer_number
-
-
-
 
 
 class bcolors:
